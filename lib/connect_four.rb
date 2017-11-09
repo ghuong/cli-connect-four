19cd34e3fc7e56181fe089f59694fa23b9f83ce8
Gary Huong <garyhuong@gmail.com>
2017-11-09 18:11:31 -0500

    RSpec for move complete

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,13 +1,27 @@
 class ConnectFour
+  MAX_HEIGHT = 6.freeze
+  NUM_COLS = 7.freeze
+
   def initialize
     @turns = 0
+    @columns = []
+    NUM_COLS.times { @columns << [] }
   end
 
   def move(column)
+    get_column(column) << get_current_player
     @turns += 1
   end
 
   def get_current_player
     (@turns % 2) + 1
+  end
+
+  def get_column(column_index)
+    @columns[column_index]
+  end
+
+  def connect_four?
+    true
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,19 +1,63 @@
 require "connect_four"
 
 describe ConnectFour do
-  describe '#move' do
-    context 'after a valid move' do
-      let!(:first_player) { subject.get_current_player }
-
-      before { subject.move(4) }
-      
-      it 'is next player\'s turn' do
-        expect(subject.get_current_player).not_to eql(first_player)
+  context 'before any moves' do
+    it 'is empty' do
+      (0...ConnectFour::NUM_COLS).each do |column_index|
+        expect(subject.get_column(column_index).length).to eql(0)
       end
     end
   end
 
-  # describe "#connect_four?" do
-    
-  # end
+  describe '#move' do
+    context 'after first valid move' do
+      let!(:first_player) { subject.get_current_player }
+      let!(:column_before_move) do
+        Marshal.load Marshal::dump(subject.get_column(column_index))
+      end
+
+      let(:column_index) { 4 }
+      let(:column) { subject.get_column(column_index) }
+
+      before { subject.move(column_index) }
+      
+      it 'is no longer first player\'s turn' do
+        expect(subject.get_current_player).not_to eql(first_player)
+      end
+
+      it 'means that column contains one piece' do
+        expect(column.length).to eql(1)
+      end
+
+      it 'means top of that column is first player\'s piece' do
+        expect(column.last).to eql(first_player)
+      end
+
+      context 'and after second valid move in the same column' do
+        let!(:second_player) { subject.get_current_player }
+
+        before { subject.move(column_index) }
+
+        it 'is first player\'s turn again' do
+          expect(subject.get_current_player).to eql(first_player)
+        end
+
+        it 'means that column contains two pieces' do
+          expect(column.length).to eql(2)
+        end
+
+        it 'means top of that column is second player\'s piece' do
+          expect(column.last).to eql(second_player)
+        end
+      end
+    end
+  end
+
+  describe "#connect_four?" do
+    context 'when four-of-a-kind in a column' do
+      it 'returns true' do
+        expect(subject.connect_four?).to be true
+      end
+    end
+  end
 end
