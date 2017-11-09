365d712af145835b6d4088c847b24213b1999690
Gary Huong <garyhuong@gmail.com>
2017-11-09 17:22:12 -0500

    Add behaviour: after valid move, it is next player's turn

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,2 +1,13 @@
 class ConnectFour
+  def initialize
+    @turns = 0
+  end
+
+  def move(column)
+    @turns += 1
+  end
+
+  def get_current_player
+    (@turns % 2) + 1
+  end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,5 +1,19 @@
 require "connect_four"
 
 describe ConnectFour do
+  describe '#move' do
+    context 'after a valid move' do
+      let!(:first_player) { subject.get_current_player }
 
+      before { subject.move(4) }
+      
+      it 'is next player\'s turn' do
+        expect(subject.get_current_player).not_to eql(first_player)
+      end
+    end
+  end
+
+  # describe "#connect_four?" do
+    
+  # end
 end
